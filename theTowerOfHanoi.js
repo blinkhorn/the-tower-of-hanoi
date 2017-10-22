714e2c90f9df27a4548b59ea34878fcbfb1cd4cd
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2017-10-22 14:35:11 -0400

    added disk divs

--- theTowerOfHanoi.js
+++ theTowerOfHanoi.js
@@ -28,7 +28,7 @@ class Disk {
 //***********************************/
 
 //generates a random color that the disk may use
-const color = function getRandomColor() {
+const generateColor = function generateRandomColor() {
   let values = "0123456789ABCDEF";
   let hash = "#";
   const HEX_DIGITS = 6;
@@ -38,7 +38,42 @@ const color = function getRandomColor() {
   return hash;
 };
 
+//adds CSS to disks as they are created in initialAddDisks()
+const diskCSS = function addDiskCSS(diskVar, disk) {
+  $(`#${diskVar}`).css({
+      background: disk.color,
+      width: disk.width,
+      height: disk.height
+    });
+}
+
+//adds disks to the tower0 rod in the beginning of the program
+const addDisks = function initialAddDisks() {
+  /* create three disks to append to #tower0's rod. Use
+  the i value from this for loop to when creating
+  the disk's size in the disk constructor above */
+  for (var i = 0; i < numberDisks; i++) {
+    const diskVar = `diskHtmlId${i}`;
+    const disk = new Disk(i, generateColor());
+    $(`<div class="disk" id="${diskVar}"></>`).appendTo("#tower0 .rod");
+    diskCSS(diskVar, disk);
+  }
+};
+
+//builds the rods and base of towers and then calls buildDisks to append disks
+//onto the first rod
+const build = function buildTowers() {
+  //append rod and base divs to each of the three towers
+  $(`<div class='rod'></>`).appendTo('.tower');
+  $(`<div class='base'></>`).appendTo('.tower');
+
+  addDisks();
+};
+
 //allows disk dragging functionality for eligible disks
 const drag = function dragDisk() {
 
 };
+
+
+build();
